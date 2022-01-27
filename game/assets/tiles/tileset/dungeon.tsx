4e866df95d406291eca854126a6440ed7af290f3
floating <gxhurry@outlook.com>
2022-01-27 15:42:48 +0800

    Init

---
+++ game/assets/tiles/tileset/dungeon.tsx
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="dungeon" tilewidth="32" tileheight="32" tilecount="128" columns="8">
+ <image source="dungeon.png" width="256" height="512"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value=""/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" value="floor"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="type" value="floor"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="type" value="floor"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="type" value="floor"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="allow" value="player"/>
+   <property name="mode" value="whitelist"/>
+   <property name="type" value="limitwall"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="type" value=""/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="type" value=""/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="type" value=""/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="type" value=""/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="type" value=""/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="type" value=""/>
+  </properties>
+ </tile>
+</tileset>
---
+++ game/world/blockDefinations.js
@@ -0,0 +1,15 @@
+export const blockDefinations = {
+    wall: {
+
+    },
+    limitwall: {
+        /**
+         * 
+         * @param {*} sprite 
+         * @param { Phaser.Tilemaps.Tile} tile 
+         */
+        collideCallback: function(sprite, tile) {
+            if (tile.properties.properties)
+        }
+    }
+};
