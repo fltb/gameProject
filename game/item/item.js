1554beccd5a3e1441d08182ec219bfa5662f78f0
floating <gxhurry@outlook.com>
2022-01-28 10:57:40 +0800

    Inited item

---
+++ game/item/item.js
@@ -0,0 +1,13 @@
+import { Entity } from "../entity/entity";
+
+export class Item {
+    constructor(){
+
+    }
+    /**
+     * @param {Entity} caller - caller of this Item
+     */
+    use(caller) {
+        
+    }
+}
