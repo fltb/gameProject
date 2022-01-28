901ea6aef61cd5bd9d6ae252c33768fba8a214fa
floating <gxhurry@outlook.com>
2022-01-28 17:50:40 +0800

    Fixed some errors

---
+++ game/world/WorldManager.js
@@ -0,0 +1,48 @@
+import { Entity } from "../entity/entity.js";
+import { blockDefinations } from "./blockDefinations.js";
+
+export class WorldManager {
+
+    /**
+     * 
+     * @param {Phaser.Scene} scene 
+     * @param {String} tilemap 
+     * @param {String} tileset 
+     * @param {Number} tileHeight
+     * @param {Number} tileWidth
+     */
+    constructor(scene, tilemap, tileset, tileWidth, tileHeight) {
+        this.scene = scene;
+        const map = this.map = scene.make.tilemap({ key: tilemap, tileWidth: tileWidth, tileHeight: tileHeight });
+        this.tileset = map.addTilesetImage(tileset);
+        
+        this.backgroundLayer = map.createLayer("background", this.tileset);
+        this.physicsLayer = map.createLayer("physics", this.tileset);
+        this.effectLayer = map.createLayer("effect", this.tileset);
+
+        // add collision to blocks, ID!
+        const BLOCK_BEGIN = 32;
+        const BLOCK_END = 53;
+        this.map.setCollisionBetween(BLOCK_BEGIN, BLOCK_END);
+
+        for (let i = BLOCK_BEGIN; i <= BLOCK_END; i++) {
+            const properties = this.tileset.getTileProperties(i);
+            if (properties) {
+                const type = properties.type;
+                if (type) {
+                    if (blockDefinations[type].collideCallback) {
+                        this.map.setTileIndexCallback(i, blockDefinations[type].collideCallback, this.scene);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 
+     * @param {Entity} entity 
+     */
+    addEntity(entity) {
+        this.scene.add.collider(entity, this.physicsLayer);
+    }
+}
--- game/world/blockDefinations.js
+++ game/world/blockDefinations.js
@@ -1,3 +1,6 @@
+import { Entity } from "../entity/entity.js";
+
+
 export const blockDefinations = {
     wall: {
 
@@ -5,11 +8,44 @@ export const blockDefinations = {
     limitwall: {
         /**
          * 
-         * @param {*} sprite 
+         * @param { Entity } sprite 
          * @param { Phaser.Tilemaps.Tile} tile 
+         * @returns {Boolean} - true means not to collide while false has opposite meaning
          */
         collideCallback: function(sprite, tile) {
-            if (tile.properties.properties)
+            
+            /**
+             * 
+             * @param {Array} original 
+             * @param {Array} checking 
+             */
+            function inArray(original, checking) {
+                if (!(original && checking)) {
+                    return false;
+                }
+                for (let i = 0; i < checking.length; i++) {
+                    const find = checking[i];
+                    for (let j = 0; j < original.length; j++) {
+                        const item = original[j];
+                        if (find === item) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+
+            if (tile.properties.mode === "whitelist") {
+                /** @type {Array} */
+                const allows = tile.properties.allows.split(",");
+                return inArray(allows, sprite.infos.states);
+            } else if (tile.properties.mode === "blacklist") {
+                /** @type {Array} */
+                const blocks = tile.properties.blocks.split(",");
+                return !inArray(blocks, sprite.infos.states);
+            } else {
+                throw new Error("At Tile ID: " + tile.index + ", A limitwall must have its mode." );
+            }
         }
     }
 };
