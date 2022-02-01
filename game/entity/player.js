4c72964c34f7bdc863c35b9aca81d4de2ee5fdc3
floating <gxhurry@outlook.com>
2022-02-01 22:44:56 +0800

    Added dash

--- game/entity/player.js
+++ game/entity/player.js
@@ -20,7 +20,15 @@ export default class Player extends Entity {
      */
     constructor(scene, x, y, texture, infos) {
         infos.type = "player";
+        /**@type {Object} - dash's info */
+        infos.dash = {
+            distance: 100,
+            cd: 500,
+            time: 150,
+        };
         super(scene, x, y, texture, infos);
+
+        this.locks = {};
 
         // remember move is not always the "WASD" key on keyboard
         scene.input.mouse.disableContextMenu();
@@ -32,6 +40,19 @@ export default class Player extends Entity {
 
     preUpdate(time, delta) {
         super.preUpdate(time, delta);
+        this.moving();
+
+        const pointer = this.scene.input.activePointer;
+        if (pointer.rightButtonDown()) {
+            this.skillDash(pointer);
+        }
+    }
+
+    moving() {
+        if (this.locks.move) {
+            return;
+        }
+
         if (this.W.isDown && !this.S.isDown) {
             this.moveUp();
         } else if (this.S.isDown && !this.W.isDown) {
@@ -50,5 +71,38 @@ export default class Player extends Entity {
         if (!(this.W.isDown || this.S.isDown || this.A.isDown || this.D.isDown)) {
             this.clearMove();
         }
+    }
+
+    /** */
+    skillDash(pointer) {
+        if (this.locks.dash) {
+            return;
+        }
+        this.locks.dash = true;
+        this.locks.move = true;
+        let that = this;
+
+        this.setVisible(false)
+
+        const  diffX = pointer.x - this.x, diffY = pointer.y-this.y;
+        const degree = Math.atan(diffY / diffX);
+        let mvX = this.infos.dash.distance * Math.cos(degree);
+        let mvY = this.infos.dash.distance * Math.sin(degree);
+        if (diffX < 0) {
+            mvX = -mvX;
+            mvY = -mvY;
+        }
+        mvX *= 1000 / this.infos.dash.time;
+        mvY *= 1000 / this.infos.dash.time;
+        
+        this.setVelocity(mvX, mvY);
+        this.scene.time.delayedCall(this.infos.dash.time, function name() {
+            that.clearMove();
+            that.setVisible(true);
+            that.locks.move = false;
+        }, [], this.scene);
+        this.scene.time.delayedCall(this.infos.dash.cd, function name() {
+            that.locks.dash = false;
+        }, [], this.scene);
     }
 }
