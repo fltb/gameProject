529f85c0301d244bf261e8dfe4c2107299fdd678
floating <gxhurry@outlook.com>
2022-02-09 17:04:45 +0800

    item

--- game/item/item.js
+++ game/item/item.js
@@ -4,10 +4,16 @@ export class Item {
 
     /**
      * 
+     * @param {Phaser.Scene} scene
+     * @param {Entity} owner
      * @param {String} texture - image path
      * @param {Number} cd - cd of this item, -1 or 0 is no
+     * @param {String} info - infos that displayed when pointer hover on image
      */
-    constructor(texture, cd){
+    constructor(scene, owner, texture, cd, info){
+        this.scene = scene;
+        this.owner = owner;
+        this.info = info;
         /**@type {String} - img path */
         this.texture = texture;
 
@@ -25,27 +31,59 @@ export class Item {
     }
     /**
      * @param {Entity} caller - caller of this Item
+     * @param {Number} toward - toword radian
      */
-    onUse(caller) {
-
+    use(caller, toward) {
+        if (this.isLocking()) {
+            return;
+        }
+        let that = this;
+        that.cd.now = that.cd.total;
+        async function cding(ms) {
+            while (that.cd.now > 0) {
+                await that.sleep(ms);
+                that.cd.now -= ms;
+                if (that.bar) {
+                    that.bar.cover.styles.height = Math.floor(that.cd.now * 100 / that.cd.total) + "%";
+                }
+            }
+            that.cd.now = 0;
+            if (that.bar) {
+                that.bar.cover.styles.height = "0";
+            }
+        }
+        cding(100);
     }
 
     /**
      * 
-     * @param {Object} bar
-     * @param {Node} bar.pic -img node
-     * @param {Node} bar.info - div with text
-     * @param {Node} bar.cover - div, change height to cover
+     * @typedef {Object} Bar
+     * @property {Node} bar.pic -img node
+     * @property {Node} bar.info - div with text
+     * @property {Node} bar.cover - div, change height to cover
+     */
+
+    /**
+     * 
+     * @param {Bar} bar
      */
     onEquip(bar) {
-
+        this.bar = bar;
+        bar.pic.setAttribute("src", this.texture);
+        bar.pic.setAttribute("title", this.info);
+        bar.info.textContent = "";
+        bar.cover.styles.height = 0;
     }
 
     /**
      * remove from bar
      */
     onRemove() {
-
+        this.bar.pic.setAttribute("src", "");
+        bar.pic.setAttribute("title", "");
+        this.bar.info.textContent = ""
+        this.bar.cover.styles.height = 0;
+        this.bar = undefined;
     }
 
     /**
@@ -53,5 +91,18 @@ export class Item {
      */
     isLocking() {
         return this.cd.now > 0;
+    }
+
+
+    /** Private functions below
+     * @param {Number} time - ms
+    */
+
+    sleep(time) {
+        return new Promise(function(resolve, reject) {
+            setTimeout(function() {
+                resolve();
+            }, time)
+        })
     }
 }
